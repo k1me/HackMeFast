e1244f0112b45d8ed4d55d48f613ab0fbebe6f86
k1me <m.kiraly21@proton.me>
2025-04-16 09:35:58 -0400

    index relocation

---
+++ frontend/hackmefast-ui/src/app/components/dashboard-info/dashboard-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-info',
+  standalone: false,
+  templateUrl: './dashboard-info.component.html',
+  styleUrl: './dashboard-info.component.css'
+})
+export class DashboardInfoComponent {
+
+}

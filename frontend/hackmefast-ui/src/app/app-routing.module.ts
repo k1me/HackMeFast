6a9766816ea58edd9be446fb2a00b2b60ac80010
k1me <m.kiraly21@proton.me>
2025-04-16 09:38:36 -0400

    új rout-ok hozzáadása

--- frontend/hackmefast-ui/src/app/app-routing.module.ts
+++ frontend/hackmefast-ui/src/app/app-routing.module.ts
@@ -1,24 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { XssComponent } from './pages/xss/xss.component';
-import { CsrfComponent } from './pages/csrf/csrf.component';
-import { SqliComponent } from './pages/sqli/sqli.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { DashboardInfoComponent } from './components/dashboard-info/dashboard-info.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { SqliComponent } from './components/sqli/sqli.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent,
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
     children: [
-      {path: 'xss', component: XssComponent},
-      {path: 'sqli', component: SqliComponent},
-      {path: 'csrf', component: CsrfComponent}
-    ]
-   }
+      {
+        path: '',
+        component: DashboardInfoComponent,
+      },
+      { path: 'xss', component: TaskListComponent, data: { category: 'XSS' } },
+      {
+        path: 'sqli',
+        component: SqliComponent,
+        data: { category: 'SQLi' },
+        children: [
+          { path: '', component: TaskListComponent },
+          { path: 'task/:id', component: TaskDetailComponent },
+        ],
+      },
+      {
+        path: 'csrf',
+        component: TaskListComponent,
+        data: { category: 'CSRF' },
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

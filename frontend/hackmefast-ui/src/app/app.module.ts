e413b873c3acd9a8bee760aa7086d76b8cbedca2
k1me <m.kiraly21@proton.me>
2025-04-16 09:38:56 -0400

    apróságok

--- frontend/hackmefast-ui/src/app/app.module.ts
+++ frontend/hackmefast-ui/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { XssComponent } from './pages/xss/xss.component';
-import { SqliComponent } from './pages/sqli/sqli.component';
-import { CsrfComponent } from './pages/csrf/csrf.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { XssComponent } from './components/xss/xss.component';
+import { SqliComponent } from './components/sqli/sqli.component';
+import { CsrfComponent } from './components/csrf/csrf.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { DashboardInfoComponent } from './components/dashboard-info/dashboard-info.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,9 @@ import { CsrfComponent } from './pages/csrf/csrf.component';
     XssComponent,
     SqliComponent,
     CsrfComponent,
+    TaskListComponent,
+    DashboardInfoComponent,
+    TaskDetailComponent,
   ],
   imports: [
     BrowserModule,

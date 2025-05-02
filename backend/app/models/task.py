5a3d47db6c04cec7e70c0278500ae4b6c5ba823e
k1me <m.kiraly21@proton.me>
2025-05-02 13:35:04 -0400

    task osztály bővítése, input db szerint elkülönítve

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,7 +1,8 @@
 class Task:
-    def __init__(self, id, title, difficulty, description, category):
-        self.id = id
+    def __init__(self, id, title, difficulty, description, category, type):
+        self.id = id # endpoint meghatarozasra, illetve, hogy hanyadik feladat
         self.title = title
         self.difficulty = difficulty
         self.description = description
-        self.category = category
+        self.category = category # maga a kategoria, hogy meg lehessen kulonboztetni a feladatokat kategoria alapjan
+        self.type = type # tipus pl.: bejelentkezos (1 parameteres, 2 parameteres, ...)  

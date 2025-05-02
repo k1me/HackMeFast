9c65c51c9ad84fff11fa32cc558d8814c668c25f
k1me <m.kiraly21@proton.me>
2025-05-02 13:55:01 -0400

    feladatleírások frissítése

--- backend/app/services/task_service.py
+++ backend/app/services/task_service.py
@@ -3,24 +3,59 @@ from app.models.task import Task
 tasks = [
     Task(
         1,
-        "SQL Injection",
-        "easy",
-        "Egy egyszerű felhasználónév‑jelszó alapú bejelentkezési űrlappal dolgozol, ahol nincsen semmilyen bemenetszűrés. A célod, hogy jelszó ismerete nélkül belépj egy tetszőleges felhasználó (például az admin) nevében.",
+        "Klasszikus Login",
+        0,
+        "Egy egyszerű bejelentkezési oldalt kaptál. Sajnos az SQL lekérdezés nem túl biztonságos... \
+            Be tudsz úgy lépni, hogy nem ismered az admin felhasználó jelszavát? Ha sikerül, kapsz egy flag-et.",
         "SQLi",
+        2,
     ),
     Task(
         2,
-        "SQL Injection",
-        "medium",
-        "Ebben a feladatban a bejelentkezési logika ugyanúgy felhasználónév‑jelszó párral dolgozik, de karaktertiltó-környezet lett bevezetve. A cél, hogy mégis sikerüljön SQL injection-nel megkerülni a bejelentkezést.",
+        "Keresés Felhasználók Közt",
+        0,
+        "Egy keresőmező segítségével kereshetsz felhasználónév alapján. Vajon van lehetőség arra, \
+            hogy más felhasználók adataihoz hozzáférj anélkül, hogy tudnád a pontos nevüket? \
+            Az egyik felhasználó adatai között találsz egy flag-et.",
         "SQLi",
+        1,
     ),
     Task(
         3,
-        "SQL Injection",
-        "hard",
-        "Egy bejelentkezési felület segítségével tudsz lekérdezni felhasználókat az adatbázisból. A szerver nem használ paraméterezett lekérdezést, hanem a username és password mezőket közvetlenül illeszti be az SQL utasításba.",
+        "Adatszivárgás UNION-nal",
+        1,
+        "Egy felhasználói ID (int) alapján lekérdezhetsz adatokat az adatbázisból. A lekérdezés viszont nem védi magát \
+            a UNION SELECT támadások ellen... \
+            Tudsz olyan adatot kinyerni, ami nem is része az eredeti lekérdezésnek?",
         "SQLi",
+        1,
+    ),
+    Task(
+        4,
+        "Blind SQLi - Boolean alapú",
+        2,
+        "A válasz minden esetben ugyanaz, de a háttérben különbség van: vajon meg tudod állapítani, hogy egy adott "
+        "felhasználó létezik-e, pusztán a válaszüzenetek alapján?",
+        "SQLi",
+        1,
+    ),
+    Task(
+        5,
+        "WAF Bypass",
+        3,
+        "Az alkalmazás egy egyszerű tűzfalat (WAF-ot) használ, ami kiszűri a jól ismert SQL kulcsszavakat. "
+        "Ennek ellenére létezik mód arra, hogy megkerüld a szűrőt, és bejuss a rendszerbe?",
+        "SQLi",
+        2,
+    ),
+    Task(
+        6,
+        "Jelszómódosítás",
+        2,
+        "Egy naplózó funkció egyetlen bemenetet vesz át – de ezt nem megfelelően szűri. Meg tudod változtatni "
+        "a sysadmin felhasználó jelszavát anélkül, hogy hitelesítve lennél?",
+        "SQLi",
+        1,
     ),
 ]
 

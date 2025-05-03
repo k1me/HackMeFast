996033e0dd4361cf914f3e9e3817be84838be43e
k1me <m.kiraly21@proton.me>
2025-05-03 17:23:38 -0400

    új konstansok meghatározása

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,6 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, "database", "users.db")
+DB_PATH = os.path.join(BASE_DIR, "database", "db")
+SECRETS_FILE = os.path.join(BASE_DIR, "database", "generated_secrets.json")
+NUM_SQLI_TASKS = 6
